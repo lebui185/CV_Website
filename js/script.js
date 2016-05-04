39aedcdc61c59737ea5eb1946d3c472f584bb6a5
Le Bui <lebui185@gmail.com>
2016-05-04 10:51:53 +0700

    Sửa thông tin Overview, Summary. Thêm thông tin phần Experience

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-var profileApp = angular.module("profileApp", []);
+var profileApp = angular.module("profileApp", ['ngSanitize']);
 
 profileApp.controller("profileController", ['$scope', '$http', function($scope, $http) {
+	window.sc = $scope.profile;
+	
 	$http.get('data/profile.json').success(function(data) {
 		$scope.profile = data;
 	});
 }]);
+
+profileApp.filter("nl2br", function($filter) {
+	return function(data) {
+		if (!data) return data;
+			return data.replace(/\n\r?/g, '<br/>');
+	};
+});
