58c60d61d3da240d71d65979a374c5a05d03e52b
Le Bui <lebui185@gmail.com>
2016-05-06 21:54:32 +0700

    Thêm chỉnh sửa thông tin

--- js/script.js
+++ js/script.js
@@ -1,12 +1,238 @@
-var profileApp = angular.module("profileApp", ['ngSanitize']);
+var profileApp = angular.module('profileApp', ['ui.bootstrap', 'ngSanitize']);
 
-profileApp.controller("profileController", ['$scope', '$http', function($scope, $http) {
-	window.sc = $scope.profile;
-	
+function isEmptyOrWhiteSpace(str) {
+	return (str.length === 0 || !str.trim());
+}
+
+function isEmailLike(str) {
+	var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return re.test(str);
+}
+
+function isNumber(str) {
+	var re = /^\d+$/;
+    return re.test(str);
+}
+
+profileApp.controller("profileController", [
+	'$scope', '$http', '$uibModal',
+	function($scope, $http, $uibModal) {
+
+	window.sc = $scope;
+
+	// load data and set editor
 	$http.get('data/profile.json').success(function(data) {
 		$scope.profile = data;
+
+		$('change-picture-button').onchange = function(e){
+			console.log("xabui");
+		};
+
+		for (var i = 0; i < $scope.profile.experience.length; i++) {
+			$scope.profile.experience[i].startDate = new Date($scope.profile.experience[i].startDate);
+			$scope.profile.experience[i].endDate = new Date($scope.profile.experience[i].endDate);
+		}
+
+		for (var i = 0; i < $scope.profile.project.length; i++) {
+			$scope.profile.project[i].startDate = new Date($scope.profile.project[i].startDate);
+			$scope.profile.project[i].endDate = new Date($scope.profile.project[i].endDate);
+		}
+
+		for (var i = 0; i < $scope.profile.education.length; i++) {
+			$scope.profile.education[i].startDate = new Date($scope.profile.education[i].startDate);
+			$scope.profile.education[i].endDate = new Date($scope.profile.education[i].endDate);
+		}
+
+		// name
+	    $('#name').editable({
+	    	title: 'Enter name',
+	    	placement: 'right',
+	    	send: 'never',
+	    	value: $scope.profile.name,
+	    	'tpl': '<input type="text" style="width: 250px">',
+	    	validate: function(value) {
+	    		if (isEmptyOrWhiteSpace(value)) {
+	    			return "Please enter a value.";
+	    		}
+	    	}
+	    });
+
+    	$('#name').on('save', function(e, params) {
+    		$scope.profile.name = params.newValue;
+		});
+
+		// headline
+	    $('#headline').editable({
+	    	title: 'Enter headline',
+	    	type: 'text',
+	    	placement: 'right',
+	    	value: $scope.profile.headline,
+	    	'tpl': '<input type="text" style="width: 350px">',
+	    	validate: function(value) {
+	    		if (isEmptyOrWhiteSpace(value)) {
+	    			return "Please enter a value.";
+	    		}
+	    	}
+	    });
+
+    	$('#headline').on('save', function(e, params) {
+    		$scope.profile.headline = params.newValue;
+		});
+
+		// country
+		$('#country').editable({
+			title: 'Select country',
+	    	type: 'select',
+	    	placement: 'right',
+      		source: ["Vietnam", "Thailand"],
+	    });
+
+    	$('#country').on('save', function(e, params) {
+    		$scope.profile.country = params.newValue;
+		});
+
+		// field
+	    $('#field').editable({
+	    	title: 'Enter field',
+	    	type: 'text',
+	    	placement: 'right',
+	    	value: $scope.profile.field,
+	    	validate: function(value) {
+	    		if (isEmptyOrWhiteSpace(value)) {
+	    			return "Please enter a value.";
+	    		}
+	    	}
+	    });
+
+    	$('#field').on('save', function(e, params) {
+    		$scope.profile.field = params.newValue;
+		});
+
+    	// email
+	    $('#email').editable({
+	    	title: 'Enter email',
+	    	type: 'text',
+	    	placement: 'right',
+	    	value: $scope.profile.email,
+	    	validate: function(value) {
+	    		if (!isEmailLike(value)) {
+	    			return "Please enter an email.";
+	    		}
+	    	}
+	    });
+
+    	$('#email').on('save', function(e, params) {
+    		$scope.profile.email = params.newValue;
+		});
+
+		// phoneNumber
+	    $('#phoneNumber').editable({
+	    	title: 'Enter Phone number',
+	    	type: 'text',
+	    	placement: 'right',
+	    	value: $scope.profile.phoneNumber,
+	    	validate: function(value) {
+	    		if (!isNumber(value)) {
+	    			return "Please enter a phone number.";
+	    		}
+	    	}
+	    });
+
+    	$('#phoneNumber').on('save', function(e, params) {
+    		$scope.profile.phoneNumber = params.newValue;
+		});
+
+		// summary
+	    $('#summary').editable({
+	    	title: 'Enter summary',
+	    	type: 'textarea',
+	    	placement: 'right',
+	    	mode: 'inline',
+	    	value: $scope.profile.summary,
+	    	tpl: '<textarea style="width:1024px"></textarea>'
+	    });
+
+    	$('#summary').on('save', function(e, params) {
+    		$scope.profile.summary = params.newValue;
+		});
+	
+    	// function
+    	$scope.openExperienceModal = function(experienceItem) {
+    		var modalInstance = $uibModal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'experienceModal.html',
+				controller: 'modalController',
+				resolve: {
+				item: function () {
+			 		return experienceItem;
+				}
+			}
+    	});
+		};
+
+		$scope.openProjectModal = function(projectItem) {
+    		var modalInstance = $uibModal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'projectModal.html',
+				controller: 'modalController',
+				resolve: {
+				item: function () {
+			 		return projectItem;
+				}
+			}
+    	});
+		};
+
+		$scope.openEducationModal = function(educationItem) {
+    		var modalInstance = $uibModal.open({
+				animation: $scope.animationsEnabled,
+				templateUrl: 'educationModal.html',
+				controller: 'modalController',
+				resolve: {
+				item: function () {
+			 		return educationItem;
+				}
+			}
+    	});
+		};
+
+
+		$('#change-picture-input').on('change', function(e){
+			$scope.$apply(function() {
+		        $scope.profile.profileImage = URL.createObjectURL(e.target.files[0]);
+		    });
+		})
 	});
 }]);
+
+//modalController
+profileApp.controller('modalController', function ($scope, $uibModalInstance, item) {
+	$scope.cloneItem = jQuery.extend(true, {}, item);
+
+	$scope.close = function() {
+		$uibModalInstance.close();
+	}
+
+	$scope.saveChange = function() {
+		angular.copy($scope.cloneItem, item);
+		$uibModalInstance.close();
+	}
+});
+
+profileApp.filter("monthYearFilter", function($filter) {
+	return function(data) {
+		var monthNames = ["January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"
+		];
+        return monthNames[data.getMonth()] + ' ' + (data.getYear() + 1900);
+	};
+});
+
+profileApp.filter("yearFilter", function($filter) {
+	return function(data) {
+        return data.getYear() + 1900;
+	};
+});
 
 profileApp.filter("nl2br", function($filter) {
 	return function(data) {
